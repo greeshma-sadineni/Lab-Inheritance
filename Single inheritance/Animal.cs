89583bef75ced08894c21c998df8a2b5fa06a8f5
Rajesh Bathula <greeshma-sadineni.com>
2020-05-20 17:05:26 +0200

    first commit

---
+++ Single inheritance/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single_inheritance
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+    }
+}
---
+++ Single inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Single_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.Eat();
+            d.Bark();
+            Console.ReadKey();
+        }
+    }
+}
